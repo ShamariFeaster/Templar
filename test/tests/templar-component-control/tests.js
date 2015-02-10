80c90b02526917eb23e700025b5c6f45cd402fb8
Shamari Feaster <alistproducer2@gmail.com>
2015-02-10 16:08:42 -0500

    wip: working on test cases for various interactions between comonents and controls

--- test/tests/templar-component-control/tests.js
+++ test/tests/templar-component-control/tests.js
@@ -22,18 +22,7 @@ QUnit.frameworkLoaded(function(){
           http://bililite.com/blog/
   */
   
-  console.log(Templar._components);
-  QUnit.test('Replacement successful', function( assert ){
-    assert.equal($('#my-dialog').length, 1, 'replacement worked');
-  });
-  
-  QUnit.test('Content Replacement successful', function( assert ){
-    assert.equal($('.content').text().trim(), "Hello", 'content replacement worked');
-  });
-  
-  QUnit.test('Stylesheet injection successful', function( assert ){
-    assert.equal($('#component-style').length, 1, 'Stylesheet injection worked');
-  });
+
 });
 
 
