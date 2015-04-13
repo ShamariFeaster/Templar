7726a684f62cdc4da2a7331bfd956449cd9c5c48
Shamari Feaster <sf11@my.fsu.edu>
2015-04-13 15:22:03 -0400

    f&f bug where live adjustments to colnum attribute of column component were not working b/c the component's current postion was not being takwn into accont.

--- test/tests/demo/Modules/Components/Column.js
+++ test/tests/demo/Modules/Components/Column.js
@@ -9,20 +9,19 @@ Templar.component('column',{
   attributes : {
     colnum : function(self, val){
       val = (_.isNullOrEmpty(val)) ? 0 : val;
-      var endLeft = ((this.colWidth * val)) + (this.parentLeft - self.offsetLeft);
-      self.style.left = endLeft + 'px';
+      this.currOffsetLeft = 
+        ((this.colWidth * val)) + (this.parentLeft - self.offsetLeft + this.currOffsetLeft);
+      self.style.left = this.currOffsetLeft + 'px';
     }
   },
-  onCreate : function(DOM_component){
-    var component = this;
-    
-    Templar.done(function(){
-      component.parentWidth = DOM_component.parentNode.offsetWidth;
-      component.parentLeft = DOM_component.parentNode.offsetLeft;
-      component.colWidth = component.parentWidth / 10;
-      component.colnumVal = DOM_component.getAttribute('colnum');
-      component.attributes.colnum.call(component, DOM_component,component.colnumVal);
-    });
+  onCreate : function(self){
+    var colnum = self.getAttribute('colnum');
+    this.parentWidth = self.parentNode.offsetWidth;
+    this.parentLeft = self.parentNode.offsetLeft;
+    this.colWidth = this.parentWidth / 10;
+    this.currOffsetLeft = 0;
+    this.attributes.colnum.call(this, self,colnum);
+
   }
 });
 
