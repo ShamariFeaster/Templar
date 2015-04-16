96614ba24deed555243688a3e82ddbfce7226461
Shamari Feaster <alistproducer2@gmail.com>
2015-04-16 15:33:46 -0400

    finished sandbox testing of other possible built-in, Limitselect

--- test/tests/sandbox-pageButton/Modules/Component.js
+++ test/tests/sandbox-pageButton/Modules/Component.js
@@ -34,5 +34,42 @@ Templar.component('PageButton',{
   }
 });
 
+Templar.component('LimitSelect',{
+  templateURL : 'LimitSelect.html',
+  attributes : {
+    max : function(){self,val},
+    data : function(self, val){}
+  },
+  
+  onCreate : function(self){
+    var max = parseInt(self.getAttribute('max')) || 10,
+        indexes = [2,5,10,25,50,75,100],
+        option;
+    
+    for(var i = 0; i < indexes.length ; i++){
+      if(indexes[i] > max)
+        break;
+      option = document.createElement("option");
+      option.text = option.value = indexes[i];
+      self.appendChild(option);
+    }
+    
+    self.addEventListener('change', function(e){
+      var val = this.getAttribute('data'),
+          _ = window.structureJS.require('Util');
+      val = (!_.isNull(val)) ? val.split('.') : [];
+      if(val.length > 1){
+        var select = e.target;
+        if(_.isDef(mdl = window.Templar.getModel(val[0])) 
+            && _.isArray(mdl[val[1]])){
+            mdl[val[1]].limit = select.options[select.selectedIndex].value;
+        }
+      }
+  
+       
+    });
+    
+  }
+});
 
 });
