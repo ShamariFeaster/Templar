4debe841e75e9a434af1d42d8f3de73fc0a0bd67
shamari feaster <sfeaster@shulware.com>
2024-04-24 13:48:30 -0400

    jasmine testing scaffolded and suite creation strategy detailed

--- test/test/Spec/Basic.js
+++ test/test/Spec/Basic.js
@@ -1,15 +1,30 @@
+//https://jasmine.github.io/tutorials/your_first_suite
 Templar.done(function(){
-  let testModel;
-  describe('when song has been paused', function() {
-    beforeEach(function() {
-      testModel = Templar.getModel('test');
+    let testModel = Templar.getModel('test');
+    describe('Basic Interpolation', function() {
+        beforeEach(function() {
+            
+        });
+        //TODO: go through docs, turning features into
+        //it()'s then construct models, html and actual tests
+        it('scalar model attribute', function() {
+            let greetingText = $('#greeting').text();
+            expect(greetingText).toBe(testModel.greeting);
+
+        });
+
     });
-  
-    it('should interpolate model attribute', function() {
-      let greetingText = $('#greeting').text();
-      expect(greetingText).toBe('hello world');
-  
+
+    describe('Complex Interpolation', function() {
+        beforeEach(function() {
+            
+        });
+
+        it('scalar model attribute', function() {
+            let greetingText = $('#greeting').text();
+            expect(greetingText).toBe(testModel.greeting);
+
+        });
+
     });
-  
-  });
 });
